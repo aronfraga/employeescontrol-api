4f021e95abdbe508c08d0cbdc0cb3824bcebd41a
Aaron Fraga <aronfraga@gmail.com>
2022-12-11 15:42:55 -0300

    deploy config

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -1,11 +1,15 @@
 import "dotenv/config";
 import { connect, set } from "mongoose";
+const { DB_LOCAL, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 set('strictQuery', false);
 
+const DEPLOY_CONECTION = <string>`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+const LOCAL_CONECTION = <string>DB_LOCAL;
+
 async function dbConnect(): Promise<void> {
-  const DB_URI = <string>process.env.DB_URI;
-  await connect(DB_URI);
+  const environmentConnection = !!DB_HOST?DEPLOY_CONECTION:LOCAL_CONECTION;
+  await connect(environmentConnection);
 }
 
 export default dbConnect;
